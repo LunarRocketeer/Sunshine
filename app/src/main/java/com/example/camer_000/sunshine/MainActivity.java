e56f6f7b0829bf54ca144c53392b7e9053da8c66
LunarRocketeer <cameron.sidney.miller@gmail.com>
2015-10-17 16:47:51 -0700

    Set up adapter.  App now displays fake data.

--- app/src/main/java/com/example/camer_000/sunshine/MainActivity.java
+++ app/src/main/java/com/example/camer_000/sunshine/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.camer_000.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] forecastArray = {
+                "Today - Sunny - 88/76",
+                "Tomorrow - Sunny - 89/82",
+                "Wednesday - Cloudy - 78/73",
+                "Thursday - Sunny - 88/79",
+                "Friday - Rainy - 80/76",
+                "Saturday - Snowy - 31/16",
+                "Sunday - Sunny - 95/83"
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, forecastArray);
+
+        ListView view = (ListView) findViewById(R.id.forecast_listview);
+        view.setAdapter(adapter);
     }
 }
